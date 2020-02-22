9329c7c0fd7367c0a18662570e17da52fb5d56ef
cgardn <christopher.gardn@gmail.com>
2020-02-22 11:49:32 -0500

    update recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,4 +9,16 @@ class RecipesController < ApplicationController
     @steps = @recipe.actions.order(order: :asc)
   end
 
+  def edit
+    @recipe = Recipe.find(params[:id])
+    @steps = @recipe.actions.order(order: :asc)
+  end
+
+  def update
+  end
+
+  private
+    def recipe_params
+      params.require(recipe).permit
+  
 end
