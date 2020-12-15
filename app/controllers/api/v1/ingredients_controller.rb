49ea0f4d70a5c9ea67d71958245d2d221553e6fd
cgardn <christopher.gardn@gmail.com>
2020-12-15 16:21:05 -0500

    fill out some missing controller actions for new api

--- app/controllers/api/v1/ingredients_controller.rb
+++ app/controllers/api/v1/ingredients_controller.rb
@@ -1,6 +1,21 @@
 module Api
   module V1
     class IngredientsController < ApplicationController
+      def index
+        render json: Ingredients.all()
+      end
+
+      def create
+      end
+
+      def show
+      end
+
+      def update
+      end
+      
+      def destroy
+      end
     end
   end
 end
--- app/controllers/api/v1/recipe_types_controller.rb
+++ app/controllers/api/v1/recipe_types_controller.rb
@@ -1,46 +1,50 @@
-class RecipeTypesController < ApplicationController
-  before_action :admin_user
-
-  def index
-    @rType = RecipeType.new
-    @rTypes = RecipeType.all.order(created_at: :desc)
-  end
-
-  def edit
-    @rType = RecipeType.find(params[:id])
-    @rTypes = RecipeType.all.order(created_at: :desc)
-  end
-
-  def create
-    @rType = RecipeType.new
-    @rType.name = rType_params[:tag]
-    if @rType.save!
-      redirect_to admin_path
-    else
-      render :new
-    end
-  end
-
-  def update
-    @rType = RecipeType.find(params[:id])
-    @rType.update_attributes(name: rType_params[:tag])
-    redirect_to admin_path
-  end
-
-  def destroy
-    @rType = RecipeType.find(params[:id])
-    @rType.delete
-    redirect_to admin_path
-  end
-
-  private
-  def rType_params
-    params.require(:recipe_type).permit(:tag)
-  end
-
-  def admin_user
-    unless user_signed_in? and current_user.admin == true
-      redirect_to root_url
+module Api
+  module V1
+    class RecipeTypesController < ApplicationController
+      before_action :admin_user
+    
+      def index
+        @rType = RecipeType.new
+        @rTypes = RecipeType.all.order(created_at: :desc)
+      end
+    
+      def edit
+        @rType = RecipeType.find(params[:id])
+        @rTypes = RecipeType.all.order(created_at: :desc)
+      end
+    
+      def create
+        @rType = RecipeType.new
+        @rType.name = rType_params[:tag]
+        if @rType.save!
+          redirect_to admin_path
+        else
+          render :new
+        end
+      end
+    
+      def update
+        @rType = RecipeType.find(params[:id])
+        @rType.update_attributes(name: rType_params[:tag])
+        redirect_to admin_path
+      end
+    
+      def destroy
+        @rType = RecipeType.find(params[:id])
+        @rType.delete
+        redirect_to admin_path
+      end
+    
+      private
+      def rType_params
+        params.require(:recipe_type).permit(:tag)
+      end
+    
+      def admin_user
+        unless user_signed_in? and current_user.admin == true
+          redirect_to root_url
+        end
+      end
     end
   end
 end
