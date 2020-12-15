3b244942f74fb50dedef559c8bc4719c6531f800
cgardn <christopher.gardn@gmail.com>
2020-12-15 16:06:45 -0500

    remove references to @rails/ujs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,15 +17,3 @@
 //console.log('Hello World from Webpacker')
 
 import TurbolinksAdapter from 'vue-turbolinks'
-import Vue from 'vue/dist/vue.esm'
-import App from '../app.vue'
-
-Vue.use(TurbolinksAdapter)
-
-Vue.component('app', App);
-
-document.addEventListener('turbolinks:load', () => {
-  const app = new Vue({
-    el: '[data-behavior="vue"]',
-  })
-})
