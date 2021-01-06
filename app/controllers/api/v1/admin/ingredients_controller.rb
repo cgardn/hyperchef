792139ccf20301e5910827f8efb1f50f07067c8c
cgardn <christopher.gardn@gmail.com>
2021-01-06 00:40:28 -0500

    ingredient update and creation

--- app/controllers/api/v1/admin/ingredients_controller.rb
+++ app/controllers/api/v1/admin/ingredients_controller.rb
@@ -6,20 +6,35 @@ class Api::V1::Admin::IngredientsController < ApplicationController
     render json: Ingredient.all.pluck(:id, :name)
   end
 
+  def show
+    if params[:id]
+      ing = Ingredient.find(params[:id])
+      iTags = ing.ingredient_tags.pluck(:id)
+    end
+    render json: {
+      ing: ing,
+      iTags: iTags,
+    }
+  end
+
   def update
     if params[:id]
       ing = Ingredient.find(params[:id])
-      ing.name = params[:name]
-      ing.caloriespergram = params[:caloriespergram]
+      ing.name = ing_params[:name]
+      ing.caloriespergram = ing_params[:caloriespergram]
+
+      # parse and update units
+      ing.units = JSON.parse(ing_params[:units])
 
       # Remove all tags, re-add the ids that were sent in
       ing.ingredient_tags.delete_all
-      params[:tags].each do |it|
+      tag_params.each do |it|
         ing.ingredient_tags << IngredientTag.find(it)
       end
 
       saveResult = ing.save
     end
+    FilterGraph.rebuild_recipes()
     FilterGraph.rebuild_filters()
     FilterGraph.rebuild_sorted_recipe_ids()
     render json: ing
@@ -27,9 +42,12 @@ class Api::V1::Admin::IngredientsController < ApplicationController
 
   private
   def ing_params
-    params.permit(:name,
-                  :caloriespergram,
-                  iTags: [:selectedIds])
+    params.require(:ing).permit(:name,
+                                :caloriespergram,
+                                :units)
+  end
+  def tag_params
+    params.require(:iTags)
   end
 
 
