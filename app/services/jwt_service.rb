0506768da0ae5a0c018504225e069447ea945c7d
cgardn <christopher.gardn@gmail.com>
2021-01-23 13:21:55 -0500

    fix incorrect key name

--- app/services/jwt_service.rb
+++ app/services/jwt_service.rb
@@ -1,12 +1,12 @@
 class JwtService
   def self.encode(payload)
-    JWT.encode(payload, ENV['JWT_SECRET'], 'HS256')
+    JWT.encode(payload, ENV['SECRET_KEY_BASE'], 'HS256')
   end
 
   def self.decode(token)
     # trailing comma skips the second returned value of JWT.decode, the
     #   headers, which we don't need
-    body, = JWT.decode(token, ENV['JWT_SECRET'],
+    body, = JWT.decode(token, ENV['SECRET_KEY_BASE'],
                        true, algorithm: 'HS256')
     HashWithIndifferentAccess.new(body)
   rescue JWT::ExpiredSignature
